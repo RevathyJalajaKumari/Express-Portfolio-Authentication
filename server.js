db83d1871e06986551553d959256670339708cda
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2023-01-18 20:22:56 -0500

    Comp229W23S401 - Week 2 End

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const http = require('http');
+const connect = require('connect');
+const app = connect();
 
-http.createServer(
-    (req, res) => {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h2>Hello World</h2>');
-    }
-).listen(3000);
+// logger middleware
+function logger(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+}
 
+function goodbyeWorld(req, res, next){
+    res.setHeader('Content-Type','text/html');
+    res.end('Goodbye World');
+}
 
+function helloWorld(req, res, next){
+    res.setHeader('Content-Type','text/html');
+    res.end('Hello World');
+}
+
+app.use(logger);
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodbyeWorld);
+
+app.listen(3000);
 
 console.log('Server running at http://localhost:3000');
