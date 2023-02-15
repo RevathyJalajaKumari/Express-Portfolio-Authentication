397703981ebf7387d45263f29ead6e015748e9ef
Revathy Jalaja Kumari <rjalajak@my.centennialcollege.ca>
2023-02-14 23:24:54 -0500

    Authentication and Authorization Completed

--- app/routes/movies.js
+++ app/routes/movies.js
@@ -1,23 +1,20 @@
 import {Router} from 'express';
 import { DisplayMoviesAddPage, DisplayMoviesEditPage, DisplayMoviesList, ProcessMoviesAddPage, ProcessMoviesDelete, ProcessMoviesEditPage } from '../controllers/movies.js';
-//import { AuthGuard } from '../utils/index.js';
+import { AuthGuard } from '../utils/index.js';
 const router = Router();
 
 // R ead
-//router.get('/movie-list', AuthGuard, DisplayMoviesList);
-router.get('/movie-list', DisplayMoviesList);
+router.get('/movie-list', AuthGuard, DisplayMoviesList);
 
 // C reate
 router.get('/movie-add', DisplayMoviesAddPage);
-//router.post('/movie-add', AuthGuard, ProcessMoviesAddPage);
-router.post('/movie-add', ProcessMoviesAddPage);
+router.post('/movie-add', AuthGuard, ProcessMoviesAddPage);
 
 // U pdate
-router.get('/movie-edit/:id', DisplayMoviesEditPage);
-//router.post('/movie-edit/:id', AuthGuard, ProcessMoviesEditPage);
-router.post('/movie-edit/:id', ProcessMoviesEditPage);
+router.get('/movie-edit/:id', AuthGuard, DisplayMoviesEditPage);
+router.post('/movie-edit/:id', AuthGuard, ProcessMoviesEditPage);
 
 // D elete
-//router.get('/movie-delete/:id', AuthGuard, ProcessMoviesDelete);
-router.get('/movie-delete/:id', ProcessMoviesDelete);
+router.get('/movie-delete/:id', AuthGuard, ProcessMoviesDelete);
+
 export default router;
