76e84d893f349d74561cae71f5a0729870fd5fae
Revathy Jalaja Kumari <rjalajak@my.centennialcollege.ca>
2023-02-14 20:32:51 -0500

    Removed node_modules and connected to Mongo Atlas

---
+++ .history/config/index_20230214201741.js
@@ -0,0 +1,3 @@
+export const Secret = "MySecret";
+//export const MongoURI = "mongodb://127.0.0.1/media"
+export const MongoURI = "mongodb://127.0.0.1/media"
---
+++ .history/config/index_20230214201844.js
@@ -0,0 +1,3 @@
+export const Secret = "MySecret";
+//export const MongoURI = "mongodb://127.0.0.1/media"
+export const MongoURI = "mongodb+srv://v7:snowflake1@cluster0.87a3ja9.mongodb.net/?retryWrites=true&w=majority"
