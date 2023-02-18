076fda9d60b62ff4a51f2f81b96dc9718452f8bf
Revathy Jalaja Kumari <rjalajak@my.centennialcollege.ca>
2023-02-17 20:49:18 -0500

    Business Contact Add and Edit page updated

--- app/models/businesscontact.js
+++ app/models/businesscontact.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const BusinessContactSchema = new Schema({
-    username: String,
+    contactname: String,
     contactnumber: Number,
     emailaddress: String
 }, {
